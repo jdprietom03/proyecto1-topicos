10325ed29b2d6b98858d1416c64c9b0f4efd4f52
Jguerra47 <juan.guerra@mercadolibre.com.co>
2023-10-15 21:55:25 -0500

    Update client to handle chunks in PUT

--- DATANODE/src/client.py
+++ DATANODE/src/client.py
@@ -8,11 +8,13 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 SERVER_ADDRESS = 'localhost:50051'
+CHUNK_SIZE = 1024 #TODO: read from config
 
 class FileClient:
     def __init__(self, address):
         self.channel = grpc.insecure_channel(address)
         self.stub = FileServices_pb2_grpc.FileServiceStub(self.channel)
+        self.chunk_count = 0
 
     def list_files(self):
         request = Empty()
@@ -25,19 +27,27 @@ class FileClient:
     def get_file(self, name):
         request = FileServicesStub.FileRequest(name=name)
         file_data = b""
-        chunk_count = 0
 
+        self.chunk_count = 0
         for chunk in self.stub.GetFile(request):
-            chunk_count += 1
-            #print(f"New chunk {chunk_count}:", chunk)
+            self.chunk_count += 1
             file_data += chunk.data
 
-        print(f"Total chunks received: {chunk_count}")
+        print(f"Total chunks received: {self.chunk_count}")
         return file_data
 
-    def put_file(self, name, data):
-        request = FileServicesStub.FileContent(name=name, data=bytes(data))
-        return self.stub.PutFile(request)
+    def put_file(self, chunks_request_generator):
+        return self.stub.PutFile(chunks_request_generator)
+    
+    def generate_chunks(self, name):
+        self.chunk_count = 0
+        with open(name, 'rb') as f:
+            while (chunk := f.read(CHUNK_SIZE)):
+                #print(f"Sending chunk: {chunk}...")
+                yield FileServicesStub.FileContent(name=name, data=chunk)
+                self.chunk_count += 1
+        print(f"Total chunks sent: {self.chunk_count}")
+            
 
 def main():
     client = FileClient(SERVER_ADDRESS)
@@ -67,10 +77,8 @@ def main():
     elif action == "put":
         name = input("Enter the name of the file to upload: ")
 
-        # Leer los datos del archivo
         try:
-            with open(name, 'rb') as f:
-                data = f.read()
+            chunks_generator = client.generate_chunks(name)
         except FileNotFoundError:
             print(f"No such file: '{name}'")
             sys.exit(1)
@@ -78,11 +86,11 @@ def main():
             print(f"Error reading file: {str(e)}")
             sys.exit(1)
 
-        status = client.put_file(name, data)
+        status = client.put_file(chunks_generator)
         print(f"Status: {status.code}, Message: {status.message}")
 
     else:
-        print("Invalid action. Usage: python3 main.py [list|find|get|put]")
+        print("Invalid action. Usage: python3 main.py [ list | find | get | put ]")
 
 if __name__ == "__main__":
     main()
